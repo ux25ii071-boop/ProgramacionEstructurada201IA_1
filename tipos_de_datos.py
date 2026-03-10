0d213507661603aebf93e59abcc868b14707181d
Bruno <brunomtzjmz30@gmail.com>
2026-03-10 08:39:52 -0600

    tipo de datos

---
+++ tipos_de_datos.py
@@ -0,0 +1,28 @@
+# Demostración de tipo de datos en Python 
+
+def datos():
+    entero = 25
+    decimal = 3.14
+    cadena = "Hola mundo!"
+    booleano = True
+
+    print(entero)
+    print(decimal)
+    print(cadena)
+    print(booleano)
+
+def tipos_datos_compuestos():
+    lista = [10, 20, 30, 40]
+    tupla = (19, 29, 39, 49)
+    diccionario = {"Nombre": "Bruno", "Edad": 18, "Ciudad": "Xalapa"}
+
+    print(lista)
+    print(tupla)
+    print(diccionario)
+
+def main():
+    datos()
+    tipos_datos_compuestos()
+
+if __name__ == "__main__":
+    main()
