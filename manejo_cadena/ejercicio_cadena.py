832725b3dd3993a2ace32780c1bd59c0afd4a748
Bruno <brunomtzjmz30@gmail.com>
2026-03-13 08:18:04 -0600

    Ejercicio cadenas

---
+++ manejo_cadena/ejercicio_cadena.py
@@ -0,0 +1,12 @@
+# Ejercicio de cadenas
+def demostracion_cadena():
+    cadena = "Universidad de Xalapa"
+    print(cadena.upper)
+    print(cadena.lower())
+    print(cadena.capitalize())
+
+def main():
+    demostracion_cadena()
+
+if __name__ == "__main__":
+    main()
--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -24,5 +24,5 @@ def main():
     datos()
     tipos_datos_compuestos()
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
